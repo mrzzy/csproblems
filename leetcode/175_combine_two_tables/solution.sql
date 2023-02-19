dd21d0705ff7b4e4a0fa836b76ad89a5cdf0d41a
Zhu Zhanyan <program.nom@gmail.com>
2023-02-19 18:23:05 +0800

    feat(leetcode): add leetcode solution to 175. Combine Two Table

---
+++ leetcode/175_combine_two_tables/solution.sql
@@ -0,0 +1,14 @@
+--
+-- CSProblems
+-- Leetcode
+-- 175. Combine Two Table
+--
+
+SELECT
+  p.firstName,
+  p.lastName,
+  a.city,
+  a.state
+FROM
+  Person p
+  LEFT JOIN Address a ON a.personId = p.personId
